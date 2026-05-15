e3121ee42a483bc022716838177d26db1ad88c96
runuplex <kyle@cardedge.co>
2026-05-15 03:06:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const crypto = require('crypto');
-
-const app = express();
-
-app.use(express.json());
-
-// Health check
-app.get('/', (req, res) => {
-  res.status(200).send('Server is running');
-});
-
-// eBay CRC / Account Deletion endpoint
 app.get('/ebay/deletion', (req, res) => {
   const challengeCode = req.query.challenge_code;
 
+  if (!challengeCode) {
+    return res.status(400).send('Missing challenge_code');
+  }
+
   const verificationToken = process.env.EBAY_VERIFICATION_TOKEN;
+
+  if (!verificationToken) {
+    return res.status(500).send('Missing EBAY_VERIFICATION_TOKEN');
+  }
 
   const endpoint = "https://ebay-deletion-endpoint-cml9.onrender.com/ebay/deletion";
 
@@ -29,35 +24,7 @@ app.get('/ebay/deletion', (req, res) => {
 
   console.log("HASH:", hash);
 
-  res.json({ challengeResponse: hash });
-});
-
-  if (!challengeCode) {
-    return res.status(400).send('Missing challenge_code');
-  }
-
-  const verificationToken = process.env.EBAY_VERIFICATION_TOKEN;
-
-  if (!verificationToken) {
-    return res.status(500).send('Missing EBAY_VERIFICATION_TOKEN');
-  }
-
-  // MUST BE STATIC for eBay
-  const endpoint = "https://ebay-deletion-endpoint-cml9.onrender.com/ebay/deletion";
-
-  const hash = crypto
-    .createHash('sha256')
-    .update(challengeCode + verificationToken + endpoint)
-    .digest('hex');
-
   return res.status(200).json({
     challengeResponse: hash
   });
-});
-
-// Start server
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
