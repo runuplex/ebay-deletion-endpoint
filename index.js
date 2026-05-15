9eadfae8ba83de3199f9979c82f5e10661f1383d
runuplex <kyle@cardedge.co>
2026-05-15 03:03:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ app.get('/', (req, res) => {
 app.get('/ebay/deletion', (req, res) => {
   const challengeCode = req.query.challenge_code;
 
+  const verificationToken = process.env.EBAY_VERIFICATION_TOKEN;
+
+  const endpoint = "https://ebay-deletion-endpoint-cml9.onrender.com/ebay/deletion";
+
+  console.log("CHALLENGE:", challengeCode);
+  console.log("TOKEN EXISTS:", !!verificationToken);
+  console.log("ENDPOINT:", endpoint);
+
+  const hash = crypto
+    .createHash('sha256')
+    .update(challengeCode + verificationToken + endpoint)
+    .digest('hex');
+
+  console.log("HASH:", hash);
+
+  res.json({ challengeResponse: hash });
+});
+
   if (!challengeCode) {
     return res.status(400).send('Missing challenge_code');
   }
